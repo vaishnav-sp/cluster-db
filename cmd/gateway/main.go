58169d17e0837ffea3e46f3e971ebdba16fbaac5
vaishnavsp <vaishnavsp.mail@gmail.com>
2026-07-08 21:35:53 +0530

    build(project): initialize Go module and gateway entrypoint

---
+++ cmd/gateway/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ClusterDB")
+	fmt.Println("Starting Gateway Service...")
+	fmt.Println("Version: dev")
+	fmt.Println("Environment: development")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vaishnav-sp/cluster-db
+
+go 1.21
